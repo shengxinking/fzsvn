f7902eb370d027015087f410e8cdb05dc9d39e35
thangguo <thangguo@43e8aa3a-562f-49c0-b7f8-d00517b08d89>
2012-12-19 14:12:01 +0000

    add some structs in mmap_file.h

--- trunk/code/system/ipc/mmap/mmap_file.h
+++ trunk/code/system/ipc/mmap/mmap_file.h
@@ -11,30 +11,63 @@
 #ifndef FZ_MMAP_SHM_H
 #define FZ_MMAP_SHM_H
 
-#define	MMAP_DIR		"/var/mmap"
-#define	MMAP_CTLFILE		MMAPDIR"/ctlfile"
+#define	MMAP_CTLFILE		"ctlfile"
 #define	MMAP_INVALID_ID		-1
+#define	MMAP_NAMELEN		16	/* the name length */
 
-typedef	int	mmap_id_t
+typedef	int			mmap_oid_t
+typedef	short			mmap_fid_t
 
+/**
+ *	The object control object 	
+ *
+ */
+typedef mmap_obj_ctl {
+	
+} mmap_obj_ctl_t;
+
+/**
+ *	the file of mmap objects.
+ */
+typedef mmap_file_ctl {
+	mmap_fid_t	fileid;		/* the file id */
+	void		*mptr;		/* mapped address */
+	size_t		len;		/* the file length */
+	size_t		objsize;	/* the object size */
+	size_t		nobj;		/* the number of objects */
+	size_t		nfreed;		/* number of freed objects */
+} mmap_file_ctl_t;
+
+/**
+ * 	the mmap control object
+ */
 typedef	mmap_ctl {
-	mmap_id_t	id;		/* mmaped id */
-	void		*mptr;		/* mmaped ptr */
+	size_t		gap;		/* the object size gap */
+	mmap_fid_t	maxid;		/* the max id of mapped files */
+	char		dir[MMAP_NAMELEN];/* the directory of mmap files */
+	int		nfile;		/* array @files number */
+	mmap_file_ctl_t	files[0];	/* mmaped files, sorted by objsize */
 } mmap_ctl_t
 
-extern int 
-mmap_create(void);
+extern mmap_ctl_t * 
+mmap_create(size_t gap, const char *dir);
 
 extern int 
-mmap_destroy(void);
+mmap_destroy(mmap_ctl_t *ctl);
 
 extern int 
-mmap_reload(void);
+mmap_load(const char *dir);
 
 extern mmap_id_t 
 mmap_alloc(size_t size);
 
+extern void * 
+mmap_id2ptr(mmap_id_t id);
+
 extern int 
+mmap_ptr2id(void *ptr);
+
+extern int  
 mmap_free(mmap_id_t id);
 
 extern int 
@@ -42,7 +75,6 @@ mmap_lock(void);
 
 extern int 
 mmap_unlock(void);
-
 
 #endif /* end of FZ_MMAP_SHM  */
 
